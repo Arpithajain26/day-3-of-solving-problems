730863903b89650961daef77f3cdacd5c9f50a5a
ARPITHA JAIN C B <arpithaammujain39@gmail.com>
2026-02-06 22:23:11 +0530

    Add files via upload

---
+++ frequencycount.py
@@ -0,0 +1,43 @@
+"""169. Majority Element
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+The input is generated such that a majority element will exist in the array.
+ """
+
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n=len(nums)
+        nums.sort()
+        return nums[n//2]
+            
+
+
+
+        
+        
